0c3621427918666f5f5b64663db3a51dbf55fa8d
Mateo Nazareno Tasso <101197950+mateotasso@users.noreply.github.com>
2022-10-05 20:57:03 -0300

    Delete Java util/src/com/bytebank directory

--- Java util/src/com/bytebank/modelo/Autenticable.java
+++
@@ -1,9 +0,0 @@
-package com.bytebank.modelo;
-
-public interface Autenticable {
-	
-	public void setClave(String clave);
-	
-	public boolean iniciarSesion(String clave);
-
-}
--- Java util/src/com/bytebank/modelo/SaldoInsuficienteException.java
+++
@@ -1,9 +0,0 @@
-package com.bytebank.modelo;
-
-public class SaldoInsuficienteException extends Exception {
-
-	public SaldoInsuficienteException(String mensaje) {
-		super(mensaje);
-	}
-	
-}
